cad1a6473cb4529936374b749f3f7e057dba2376
Alex Leschenko <leschenko.al@gmail.com>
2013-01-08 06:02:40 +0200

    fix typo

--- lib/elasticsearch_autocomplete/railtie.rb
+++ lib/elasticsearch_autocomplete/railtie.rb
@@ -1,6 +1,6 @@
 module ElasticsearchAutocomplete
   class Railtie < Rails::Railtie
-    initializer 'bit_mask.model_additions' do
+    initializer 'elasticsearch_autocomplete.model_additions' do
       ActiveSupport.on_load :active_record do
         include ElasticsearchAutocomplete::ModelAddition
       end
