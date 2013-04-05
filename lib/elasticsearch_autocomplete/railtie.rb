bed2099d180ed7aff8ab206530be689244423ca5
Alex Leschenko <leschenko.al@gmail.com>
2013-04-05 23:37:57 +0300

    add mongoid initializer hook

--- lib/elasticsearch_autocomplete/railtie.rb
+++ lib/elasticsearch_autocomplete/railtie.rb
@@ -1,9 +1,17 @@
 module ElasticsearchAutocomplete
   class Railtie < Rails::Railtie
     initializer 'elasticsearch_autocomplete.model_additions' do
-      ActiveSupport.on_load :active_record do
-        include ElasticsearchAutocomplete::ModelAddition
+
+      if defined?(Mongoid::Document)
+        ActiveSupport.on_load :mongoid do
+          Mongoid::Document.send :include, ElasticsearchAutocomplete::ModelAddition
+        end
+      else
+        ActiveSupport.on_load :active_record do
+          ActiveRecord::Base.send :include, ElasticsearchAutocomplete::ModelAddition
+        end
       end
+
     end
   end
 end
