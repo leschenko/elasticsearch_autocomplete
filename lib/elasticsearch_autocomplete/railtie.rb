38123cf375f0d883f4e768e75e5d348fccfd0ccf
Alex Leschenko <leschenko.al@gmail.com>
2013-04-05 23:41:27 +0300

    fix mongoid initializer hook

--- lib/elasticsearch_autocomplete/railtie.rb
+++ lib/elasticsearch_autocomplete/railtie.rb
@@ -4,7 +4,7 @@ module ElasticsearchAutocomplete
 
       if defined?(Mongoid::Document)
         ActiveSupport.on_load :mongoid do
-          Mongoid::Document.send :include, ElasticsearchAutocomplete::ModelAddition
+          Mongoid::Document::ClassMethods.send :include, ElasticsearchAutocomplete::ModelAddition::SingletonMethods
         end
       else
         ActiveSupport.on_load :active_record do
