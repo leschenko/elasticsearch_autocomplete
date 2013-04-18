e2624f795471d4cf697389ead32aac604b679c84
Alex Leschenko <leschenko.al@gmail.com>
2013-04-18 12:15:02 +0300

    removed check for Mongoid::Document constant in railtie

--- lib/elasticsearch_autocomplete/railtie.rb
+++ lib/elasticsearch_autocomplete/railtie.rb
@@ -2,14 +2,12 @@ module ElasticsearchAutocomplete
   class Railtie < Rails::Railtie
     initializer 'elasticsearch_autocomplete.model_additions' do
 
-      if defined?(Mongoid::Document)
-        ActiveSupport.on_load :mongoid do
-          Mongoid::Document::ClassMethods.send :include, ElasticsearchAutocomplete::ModelAddition::SingletonMethods
-        end
-      else
-        ActiveSupport.on_load :active_record do
-          ActiveRecord::Base.send :include, ElasticsearchAutocomplete::ModelAddition
-        end
+      ActiveSupport.on_load :mongoid do
+        Mongoid::Document::ClassMethods.send :include, ElasticsearchAutocomplete::ModelAddition::SingletonMethods
+      end
+
+      ActiveSupport.on_load :active_record do
+        ActiveRecord::Base.send :include, ElasticsearchAutocomplete::ModelAddition
       end
 
     end
