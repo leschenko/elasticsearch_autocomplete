b3de42f265bca49d60ef2f430a5add25c12feea8
Alex Leschenko <leschenko.al@gmail.com>
2016-01-28 10:32:59 +0200

    Rspec tire config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,8 @@ require 'active_support/core_ext'
 require 'elasticsearch_autocomplete'
 
 Tire.configure do
-  #logger 'tmp/elasticsearch.log'  # Commented out logger line here so that it doesn't break specs when tmp directory doesn't exist.
-  url 'http://localhost:9200'
+  # logger 'tmp/elasticsearch.log'  # Commented out logger line here so that it doesn't break specs when tmp directory doesn't exist.
+  url "http://localhost:#{ENV['ES_PORT'] || 9200}"
   pretty 1
 end
 
